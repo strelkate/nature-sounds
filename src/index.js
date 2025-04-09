e0e3175b705ede3468f0f08aa25c1e2c49c5f869
e.vasileva <e.vasileva@3h.ru>
2025-04-09 10:49:42 +0300

    fix: add icon change on click

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const sounds = {
 let currentAudio = null
 let isPlaying = false
 let currentKey = null
+let currentIcon = null
 
 const buttons = document.querySelectorAll('[data-sound]')
 const volumeSlider = document.getElementById('volume')
@@ -20,15 +21,18 @@ const wrapper = document.querySelector('.wrapper')
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const key = button.dataset.sound
+    const icon = button.querySelector('img')
 
     // Same button toggles
     if (currentKey === key && currentAudio) {
       if (isPlaying) {
         currentAudio.pause()
         isPlaying = false
+        icon.src = `../assets/icons/${key}.svg`
       } else {
         currentAudio.play()
         isPlaying = true
+        icon.src = `../assets/icons/pause.svg`
       }
       return
     }
@@ -37,6 +41,7 @@ buttons.forEach(button => {
     if (currentAudio) {
       currentAudio.pause()
       currentAudio.currentTime = 0
+      currentIcon.src = `../assets/icons/${currentKey}.svg`
     }
 
     // Play new
@@ -47,6 +52,8 @@ buttons.forEach(button => {
     isPlaying = true
     currentKey = key
 
+    icon.src = `../assets/icons/pause.svg`
+    currentIcon = icon
     wrapper.style.backgroundImage = `url('../assets/images/${key}-bg.jpg')`
 
   })
